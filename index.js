c254e3cdd15cfed4f1be219369a313346518ce1e
Andy Chadha <andy.chadha2@gmail.com>
2019-05-04 02:13:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,12 +22,12 @@ class BoardMember {
   }
   
   sayHi(){
-    return `Hi, my name is ${name}. I am from ${homestate}, and I was trained in ${training}`
+    return `Hi, my name is ${name}. I am from ${homeState}, and I was trained in ${training}`
   }
 }
 
 class Ceo extends BoardMember {
   hireEmployee(){
-    return "Welcome Aboard!"
+    return "Welcome aboard!"
   }
 }
